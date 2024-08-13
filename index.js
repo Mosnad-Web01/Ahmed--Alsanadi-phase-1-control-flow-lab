e1719aa53f4d1a222c2f7dfe1c28094b158a71ae
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-13 03:30:56 +0300

    completed test

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numFeet) {
+  if (numFeet <= 400) {
+    return 'This one is on me!';
+  } else if (numFeet > 400 && numFeet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (numFeet > 2000 && numFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
